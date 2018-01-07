33fffa3ee2682573babf8eb0c34b37f1f6c36e7d
Ricky Raghavan <rajivsr@yahoo.com>
2018-01-06 22:56:37 -0500

    Added React Code.

--- react.js
+++ react.js
@@ -29,5 +29,21 @@
       * https://reactjs.org/docs/react-without-jsx.html
       * https://reactjs.org/docs/cdn-links.html
     -->
+
+    class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
   </body>
 </html>
