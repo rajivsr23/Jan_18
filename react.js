34aa8ec16e6ac87fa63af71e75bba0558678108c
Ricky Raghavan <rajivsr@yahoo.com>
2018-01-22 21:49:11 -0500

    Three Llines of code

--- react.js
+++ react.js
@@ -44,6 +44,9 @@
     );
   }
 }
-
+return React.createElement('div', {className: 'shopping-list'},
+  React.createElement('h1', /* ... h1 children ... */),
+  React.createElement('ul', /* ... ul children ... */)
+);
   </body>
 </html>
