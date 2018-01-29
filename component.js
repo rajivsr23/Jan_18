27122f3ece88e4d70c506351a9bf2f7b08bf8b17
Ricky Raghavan <rajivsr@yahoo.com>
2018-01-28 21:05:00 -0500

    MarkdownEditor

--- component.js
+++ component.js
@@ -27,3 +27,19 @@ class Board extends React.Component {
   renderSquare(i) {
     return <Square value={i} />;
   }
+
+  class MarkdownEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { value: 'Type some *markdown* here!' };
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  getRawMarkup() {
+    const md = new Remarkable();
+    return { __html: md.render(this.state.value) };
+  }
